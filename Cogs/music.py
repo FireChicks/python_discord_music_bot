d39d877350549335528979ff41cd2db35ee4d569
FireChicks <dldi991021@gmail.com>
2024-01-19 00:58:52 +0900

    버그 수정

--- Cogs/music.py
+++ Cogs/music.py
@@ -1,3 +1,4 @@
+import functools
 import queue
 import random
 import time
@@ -171,7 +172,8 @@ class music(commands.Cog):
                         music_name = que['path']
                         source = discord.PCMVolumeTransformer(FFmpegPCMAudio(music_name))
                         self.now_music_name = music_name
-                        await voice_client.play(source, after=after_play)
+                        await voice_client.play(source, after=functools.partial(self.after_play,
+                                                                                guild=guild))  # functools.partial로 함수와 인자 전달
                         voice_client.source.volume = self.volume / 100
                         target_channel = self.bot.get_channel(self.target_channel_id)
                         msg = (
@@ -185,28 +187,31 @@ class music(commands.Cog):
 
                 source = discord.PCMVolumeTransformer(FFmpegPCMAudio(music_name))
                 self.now_music_name = music_name
-                await voice_client.play(source, after=after_play)
+                await voice_client.play(source, after=functools.partial(after_play))  # functools.partial로 함수와 인자 전달
                 voice_client.source.volume = self.volume / 100
             else:
                 self.queue = queue.Queue()
                 await self.send_music_info("음성채널에 연결되어있지 않습니다.")
 
-    async def after_play(self, guild):
-        # 재생이 끝난 음성 파일을 제거하고 다음 메시지를 재생합니다.
+    async def after_play(self, error, guild):  # async 함수로 변경
         que = self.queue.get()
         music_name = que['path']
         name = que['author']
-
 
         if not self.queue.empty():
             voice_client = guild.voice_client
             voice_client.stop()
             source = discord.PCMVolumeTransformer(FFmpegPCMAudio(music_name))
             self.now_music_name = music_name
-            voice_client.play(source, after=self.after_play)
+            await voice_client.play(source, after=functools.partial(self.after_play,
+                                                                    guild=guild))  # functools.partial로 함수와 인자 전달
             voice_client.source.volume = self.volume / 100
-            str = "**" + name + "**가 추가한 다음 노래 **" + music_name.split('/')[1].split('.')[0] + "**가 재생됩니다."
-            await self.send_music_info(str)
+            msg = (
+                    "**" + name + "**가 추가한 다음 노래 **" +
+                    music_name.split('/')[1].split('.')[0] +
+                    "**가 재생됩니다."
+            )
+            await self.send_music_info(msg)
         else:
             await self.send_music_info("더이상 재생할 노래가 없습니다.")
 
