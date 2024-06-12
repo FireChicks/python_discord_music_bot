5dca79ce025e831066f32902dd19337e71020573
FireChicks <dldi991021@gmail.com>
2024-06-13 02:38:58 +0900

    버그 수정

--- Cogs/music.py
+++ Cogs/music.py
@@ -388,7 +388,7 @@ class music(commands.Cog):
                 for file_name in self.found_files:
                     queue_part = {'path': self.downloadPath + file_name, 'author': '랜덤 추가'}
                     self.queue.put(queue_part)
-                    await interaction.response.send_message("다음 노래 " + file_name + "을 재생합니다.")
+                    await self.play_next_music(interaction.guild)
 
             await interaction.response.send_message("현재 플레이리스트에 다음 노래가 없습니다.")
         await interaction.response.send_message("다음 노래를 시작합니다.")
@@ -516,6 +516,12 @@ class music(commands.Cog):
             if self.queue.qsize() > 0 :
                 await self.play_next_music(interaction.guild)
             else :
+                if self.isRandomAppend:
+                    self.found_files = self.search_random_music(1)
+                    for file_name in self.found_files:
+                        queue_part = {'path': self.downloadPath + file_name, 'author': '랜덤 추가'}
+                        self.queue.put(queue_part)
+                        await self.play_next_music(interaction.guild)
                 await interaction.response.send_message("플레이리스트에 다음 노래가 없습니다.",view=view)
 
             await interaction.message.delete()
